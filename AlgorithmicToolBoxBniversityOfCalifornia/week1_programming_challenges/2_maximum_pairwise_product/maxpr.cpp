308a595fe4d8ac293c0858ac30a0910b178e051a
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-11-28 23:32:15 +0530

    updated maxpr.cpp
    updated maxpr by sorting all the elements of the array and instead of looping just multiplied the last two elements of the sorted array.
    A better O(n) implementation was to track the two max by looping through the array twice.Although my submission was successful so didn't bother to do that.

--- AlgorithmicToolBoxBniversityOfCalifornia/week1_programming_challenges/2_maximum_pairwise_product/maxpr.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week1_programming_challenges/2_maximum_pairwise_product/maxpr.cpp
@@ -4,15 +4,15 @@ int main()
 {
 int n;
 cin>>n;
-int a[n];
+long long int a[n];
 for(int i=0;i<n;i++)
 cin>>a[i];
-long int maxpr =0;
-for(int i=0;i<n-1;i++)
-  for(int j=i+1;j<n;j++)
-    {
-	   maxpr = max(maxpr,a[i]*a[j]); 
-	}
+
+long long int maxpr =0;
+sort(a,a+n);
+maxpr = a[n-1]*a[n-2];
 cout<<maxpr<<endl;
+
+
 return 0;
 }
