c95dff2e4695b1b72b986ffa1cc2a9527b6fc02b
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-11-29 19:33:25 +0530

    week 2 assignment
    1. Fibonacci using dynamic programming
    2. Finding the last digit of a fibonacci for a large value of n, also implemented using dynamic programming.
    3. Finding efficiently the gcd of two numbers.
    4. Finding the lcm of two numbers using the gcd .

---
+++ AlgorithmicToolBoxBniversityOfCalifornia/week2_algorithmic_warmup/1_fibonacci_number/fibonaccidp.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   long long int a[n+1];
+   a[0] = 0;
+   a[1] = 1;
+   for(int i=2;i<=n;i++)
+     a[i] = a[i-1] + a[i-2];
+    cout<<a[n]<<endl;
+    
+    
+    
+    return 0;
+}
---
+++ AlgorithmicToolBoxBniversityOfCalifornia/week2_algorithmic_warmup/3_greatest_common_divisor/gcdab.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int gcdab(long long int a ,long long int b)
+{
+    if(b==0)
+     return a;
+ return gcdab(b,a%b);
+}
+int main()
+{
+   long long int a,b;
+   cin>>a>>b;
+   long long int c;
+   c = gcdab(a,b);
+   cout<<c<<endl;
+    
+    
+    
+    return 0;
+}
