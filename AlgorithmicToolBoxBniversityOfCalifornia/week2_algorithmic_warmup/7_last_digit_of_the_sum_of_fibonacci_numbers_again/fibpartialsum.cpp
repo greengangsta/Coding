4ed0453f35a79799170c2b45172697f208a6d433
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-11-30 18:47:43 +0530

    summing the last digit of fibonacci numbers within a given range and output the last digit of the result obtained.
    The last digit of fibonacci no also has a period of 60.
    Also
    sum(Fn) = Fn+2 - 1

---
+++ AlgorithmicToolBoxBniversityOfCalifornia/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibpartialsum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   
+   long long int m,n,d,dm;
+   cin>>m>>n;
+   d = (n-m)%60;
+   dm = m%60;
+   long long int a[62],sum =0,b[62];
+   a[0] = 0;
+   a[1] = 1;
+   for(int i=2;i<61;i++)
+    {
+        a[i] = a[i-1]+a[i-2];
+        b[i] = a[i]%10;
+        
+    }
+    sum =b[dm]%10;
+  for(int i =dm+1;i<=dm+d;i++)
+     {
+         if(i>=60)
+           i =0;
+         sum += b[i]%10;
+         
+     }
+    cout<<sum%10<<endl;
+
+    return 0;
+}
