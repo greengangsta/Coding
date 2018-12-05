b8c342e1c95327f58d5a6752d3cd01dd447a9f35
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-12-05 22:09:19 +0530

    fractional knapsack

--- AlgorithmicToolBoxBniversityOfCalifornia/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -3,10 +3,35 @@
 
 using std::vector;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+double get_optimal_value(int capacity, vector<double> weights, vector<double> values) {
   double value = 0.0;
-
-  // write your code here
+  int n  = weights.size();
+  while(capacity>0)
+  { 
+       double max = 0;
+       int id = -1;
+	  for(int i = 0;i<n;i++)
+	   {
+		 if( values[i]/weights[i]>max)
+		 {
+			 max = values[i]/weights[i];
+		      id = i;
+		 } 
+	   }
+	   if(capacity >= weights[id])
+	   {
+		   capacity -= weights[id];
+		   value += values[id];
+		   values[id] = 0;
+	   }
+      else if (capacity > 0){
+		  double m = 0.0;
+		  m =(values[id]/weights[id])*capacity;
+		  value +=m;
+		  capacity = 0;
+	  }
+  }
+  
 
   return value;
 }
@@ -15,8 +40,8 @@ int main() {
   int n;
   int capacity;
   std::cin >> n >> capacity;
-  vector<int> values(n);
-  vector<int> weights(n);
+  vector<double> values(n);
+  vector<double> weights(n);
   for (int i = 0; i < n; i++) {
     std::cin >> values[i] >> weights[i];
   }
