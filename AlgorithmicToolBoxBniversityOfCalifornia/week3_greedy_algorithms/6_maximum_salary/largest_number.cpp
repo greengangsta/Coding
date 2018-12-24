eb59163cbac7eb9d04bcc4d725362c15088e2869
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-12-24 17:34:34 +0530

    solved largest number

--- AlgorithmicToolBoxBniversityOfCalifornia/week3_greedy_algorithms/6_maximum_salary/largest_number.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week3_greedy_algorithms/6_maximum_salary/largest_number.cpp
@@ -1,14 +1,21 @@
-#include <algorithm>
-#include <sstream>
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 
-using std::vector;
-using std::string;
+using namespace std;
 
+bool cmp(string a,string b)
+{    
+    string p = a+b;
+    string q = b+a;
+    stringstream s1(p);
+    stringstream s2(q);
+   	 int n1 , n2 =0;
+   	 s1>>n1;
+   	 s2>>n2;
+   	 return n1>n2;
+}
 string largest_number(vector<string> a) {
   //write your code here
+  sort(a.begin(),a.end(),cmp);
   std::stringstream ret;
   for (size_t i = 0; i < a.size(); i++) {
     ret << a[i];
