2cdb47169980e4f572e9aa4c9f9c4b80899e5e72
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-12-25 18:50:30 +0530

    solved majoity element using divide and conquer approach

--- AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -4,10 +4,23 @@
 
 using std::vector;
 
+
 int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
+  int m,le,re,lc=0,rc=0;
+   m = (left+right-1)/2 +1;
+   le = get_majority_element(a,left ,m);
+  re = get_majority_element(a,m,right);
+   for(int i=left;i<right;i++)
+	   if(a[i]==le)
+		   lc++;
+   for(int i=left;i<right;i++)
+      if(a[i]==re)
+          rc++;		  
+   if(rc > (right-left/2))
+	   return re;
   return -1;
 }
 
