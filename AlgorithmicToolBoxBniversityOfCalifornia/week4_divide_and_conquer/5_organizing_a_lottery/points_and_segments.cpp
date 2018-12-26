5c0aefea5ddeb4ed357c55bddb358ca98a60211e
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-12-26 21:07:59 +0530

    Solving the points and segments using binary search.
    Calculating the no of starting points to the left of the point and no of end points to the right of the point.
    then applying
    c = s+e-n
    where n = no of total segments

--- AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
@@ -1,11 +1,53 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
 
 using std::vector;
+
+int bin_src_count(vector <int> a,int p)
+{
+	int n=a.size();
+	int l=0,r=n-1;
+	int m;
+	while(l<=r){
+	int m = l + (r-l)/2;
+	if(a[m]<=p)
+		l=m+1;
+	else
+		r= m-1;
+	}
+	
+	return l;
+}
+int bin_src_count1(vector <int> a,int p)
+{
+	int n=a.size();
+	int l=0,r=n-1;
+	int m;
+	while(l<=r){
+	int m = l + (r-l)/2;
+	if(a[m]<=p)
+		l=m+1;
+	else
+		r= m-1;
+	}
+	
+	return n-l;
+}
 
 vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
   vector<int> cnt(points.size());
   //write your code here
+  int n=starts.size();
+  sort(starts.begin(),starts.end());
+  sort(ends.begin(),ends.end());
+  for(int i=0;i<points.size();i++)
+   {
+	   int s,e;
+	  s = bin_src_count(starts,points[i]);
+	  e = bin_src_count1(ends,points[i]);
+	//  cout<<s<<" s and e "<<e<<endl;
+	  cnt[i]= s+e-n;
+   }
   return cnt;
 }
 
@@ -26,13 +68,14 @@ int main() {
   for (size_t i = 0; i < starts.size(); i++) {
     std::cin >> starts[i] >> ends[i];
   }
-  vector<int> points(m);
+  vector <int> points(m);
   for (size_t i = 0; i < points.size(); i++) {
     std::cin >> points[i];
   }
   //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
+  vector<int> cnt = fast_count_segments(starts, ends, points);
   for (size_t i = 0; i < cnt.size(); i++) {
     std::cout << cnt[i] << ' ';
   }
+
 }
