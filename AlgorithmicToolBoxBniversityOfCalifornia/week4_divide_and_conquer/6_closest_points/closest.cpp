fb287b320b816624e29f068acdd3b93503414940
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-12-27 15:30:06 +0530

    update closest point

--- AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/6_closest_points/closest.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/6_closest_points/closest.cpp
@@ -1,20 +1,69 @@
-#include <algorithm>
-#include <iostream>
-#include <sstream>
-#include <iomanip>
-#include <vector>
-#include <string>
-#include <cmath>
+#include<bits/stdc++.h>
+using namespace std;
 
-using std::vector;
-using std::string;
-using std::pair;
-using std::min;
+double dist(pair<int,int> a,pair <int,int> b)
+{
+	return sqrt((a.first -b.first)*(a.first -b.first) +
+	(a.second -b.second)*(a.second -b.second));
+}
+double brut_frc(vector <int,int> ps,n)
+{
+	double min = DOUBLE_MAX;
+	for(int i=0;i<n;i++)
+		for(int j=i+1;j<n;++j)
+			 if(dist(ps[i],ps[j])<min)
+				 min = dist(ps[i],ps[j]);
+	return min;
+}
+bool cmp1(const pair<int,int> &a,const pair<int,int > &b)
+{
+	return a.first < b.first;
+}
+bool cmp2(const pair<int,int> &a,const pair <int,int> &b)
+{
+	return a.second < b.second;
+}
+double stripclosest(vector <pair <int,int> > strip,j,d)
+{
+	double min = d;
+	sort(strip.begin(),strip.end();cmp2);
+	for(int i=0;i<j;i++)
+	  for(int k=i+1;k<j&&strip[k].second -strip[i].second<min;k++)
+		  if(dist(strip[i],strip[j]<min)
+			  min=dist(strip[i],strip[j]);
+   return min;
+}
 
 
 double minimal_distance(vector<int> x, vector<int> y) {
+	 vector <pair <int,int > > ps;
+	for(int i=0;i<x.size();i++)
+	   {
+		   ps[i].first=x[i];
+		   ps[i].second = y[i].
+	   }
   //write your code here
-  return 0.;
+	  sort(ps.begin(),ps.end(),cmp1);
+	  int n = ps.size();
+	  if(n<=3)
+		 return brut_frc(ps,n);
+	  int m =n/2;
+	  pair<int,int> midpoint = ps[m];
+	  float dl = minimal_distance(ps,m);
+	  float dr = minimal_distance(ps+m,n-m)
+	   
+	   float d = min(dl,dr);
+	  vector <pair <int,int > > strip(n);
+	  int j=0;
+	  for(int i=0;i<n;i++)
+		  if(abs(ps[i].first -midpoint.first < d)
+		  {
+			  strip[j].first = ps[i].first;
+			  strip[j].second = ps[i].second;
+			  j++;
+		  }
+	   
+  return min (d,stripclosest(strip,j,d);
 }
 
 int main() {
