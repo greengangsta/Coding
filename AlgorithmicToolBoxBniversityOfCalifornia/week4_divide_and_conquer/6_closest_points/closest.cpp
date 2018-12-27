fa22cb158af078030aa41e2d16ff570bcd9bf843
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-12-27 21:43:12 +0530

    two separate sets of points
    one sorted according to x co-ordinate
    another sorted according to y co-ordinate.

--- AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/6_closest_points/closest.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/6_closest_points/closest.cpp
@@ -1,59 +1,91 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-double dist(pair<int,int> a,pair <int,int> b)
-{
-	return sqrt((a.first -b.first)*(a.first -b.first) +
-	(a.second -b.second)*(a.second -b.second));
-}
-double brut_frc(vector <pair<int,int> >ps,int n)
-{
-	double min = FLT_MAX;
-	for(int i=0;i<n;i++)
-		for(int j=i+1;j<n;++j)
-			 if(dist(ps[i],ps[j])<min)
-				 min = dist(ps[i],ps[j]);
-	return min;
-}
 bool cmp1(const pair<int,int> &a,const pair<int,int > &b)
 {
-	return a.first < b.first;
+    return a.first <= b.first;
 }
 bool cmp2(const pair<int,int> &a,const pair <int,int> &b)
 {
-	return a.second < b.second;
+	return a.second <= b.second;
 }
+double min(double x,double y)
+{
+    return (x<y)?x:y;
+}
+double dist(const pair<int,int> &a,const pair <int,int> &b)
+{
+    cout<<"check dist"<<endl;
+	return sqrt(
+	             (a.first -b.first)*(a.first -b.first) +
+     	         (a.second -b.second)*(a.second -b.second)
+            	);
+}
+double brut_frc(vector <pair<int,int> >ps)
+{
+    cout<<"check brut_frc"<<endl;
+	double min = 100000.000000;
+	for(int i=0;i<ps.size();i++)
+		for(int j=i+1;j<ps.size();j++)
+			 if(dist(ps[i],ps[j])<min)
+				 min = dist(ps[i],ps[j]);
+   cout<<"min ="<<min<<endl;
+	return min;
+}
+
 double stripclosest(vector <pair <int,int> > strip,int j,int d)
 {
+    cout<<"check stripclosest"<<endl;
 	double min = d;
-	sort(strip.begin(),strip.end(),cmp2);
-	for(int i=0;i<j;i++)
-	  for(int k=i+1;k<j&&strip[k].second -strip[i].second<min;k++)
-		  if(dist(strip[i],strip[j])<min)
-			  min=dist(strip[i],strip[j]);
+	for(int i=0;i<j;++i)
+	  for(int k=i+1;k<j && (strip[k].second -strip[i].second)<min;++k)
+		  if(dist(strip[i],strip[k])<min)
+			  min=dist(strip[i],strip[k]);
+			  cout<<"min from strip closest is : "<<min<<endl;
    return min;
 }
 
 
-double minimal_distance(vector<pair<int,int> > ps, int  l,int n) {
+double minimal_distance(vector<pair<int,int>> px, vector <pair<int ,int>> py) {
   //write your code here
-	  sort(ps.begin(),ps.end(),cmp1);
+  cout<<"check minimal_distance"<<endl;
+  int  n = px.size();
 	  if(n<=3)
-		 return brut_frc(ps,n);
-	  int m =n/2;
-	  pair<int,int> midpoint = ps[m];
-	  double dl = minimal_distance(ps,l,m);
-	  double dr = minimal_distance(ps,m+1,n);
-	   
+		 return brut_frc(px);
+	   int m = n/2;
+	  cout<<"mid point of partition is : "<<m<<endl;
+	  pair<int,int> midpoint = px[m];
+	  cout<<"mid point co-ordinates are : "<<midpoint.first<<" "<<midpoint.second<<endl;
+	  vector <pair<int,int>> qx(m+1),qy(m+1);
+	  vector <pair<int,int>> rx(n-m-1),ry(n-m-1);
+	  int j=0,k=0;
+	    for(int i=0;i<py.size();i++)
+	      {
+	          if(py[i].first<=midpoint.first)
+	             qy[j++]=py[i];
+	          else
+	             ry[k++]=py[i];
+	      }
+	      j=0;
+	    for(int i=0;i<=m;i++)
+	         qx[i] = px[i];
+	    for(int i=m+1;i<n;i++)
+	         rx[j++] =px[i];
+	  double dl = minimal_distance(qx,qy);
+	  double dr = minimal_distance(rx,ry);
+	   cout<<"left min is = "<<dl<<endl;
+	   cout<<"right min is = "<<dr<<endl;
 	   double d = min(dl,dr);
+	   cout<<"d : "<<d<<endl;
 	  vector <pair <int,int > > strip(n);
-	  int j=0;
+	   j=0; 
 	  for(int i=0;i<n;i++)
-		  if(abs(ps[i].first -midpoint.first < d))
+		  if(abs(py[i].first-midpoint.first) <= d)
 		  {
-			  strip[j].first = ps[i].first;
-			  strip[j].second = ps[i].second;
+			  strip[j].first = py[i].first;
+			  strip[j].second = py[i].second;
 			  j++;
+			  cout<<"strip recently added is : "<<py[i].first<<" "<<py[i].second<<endl;
 		  }
 	   
   return min (d,stripclosest(strip,j,d));
@@ -62,12 +94,18 @@ double minimal_distance(vector<pair<int,int> > ps, int  l,int n) {
 int main() {
   size_t n;
   std::cin >> n;
-  vector<pair<int,int>> x(n);;
+  vector<pair<int,int>> px(n),py(n);
   for (size_t i = 0; i < n; i++) {
-    std::cin >> x[i].first >> x[i].second;
+    std::cin >> px[i].first >> px[i].second;
+    py[i].first = px[i].first;
+    py[i].second = px[i].second;
+    
   }
-  for(int i=0;i<n;i++)
-    cout<<x[i].first<<" "<<x[i].second<<endl;
+
   std::cout << std::fixed;
-  std::cout << std::setprecision(9) << minimal_distance(x,0,n) << "\n";
+  	  sort(px.begin(),px.end(),cmp1);
+  	  sort(py.begin(),py.end(),cmp2);
+  	      for(int i=0;i<n;i++)
+    cout<<py[i].first<<" "<<py[i].second<<endl;
+  std::cout << std::setprecision(9) << minimal_distance(px,py) << "\n";
 }
