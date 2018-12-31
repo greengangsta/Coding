52bb981798c6fdd3968b6166e4cb8f8a5951dde0
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-12-31 20:57:07 +0530

    coin change problem

--- AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/6_closest_points/tmplt.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week4_divide_and_conquer/6_closest_points/tmplt.cpp
@@ -1,30 +1,70 @@
-#include <algorithm>
-#include <iostream>
-#include <sstream>
-#include <iomanip>
-#include <vector>
-#include <string>
-#include <cmath>
+#include<bits/stdc++.h>
+using namespace std;
 
-using std::vector;
-using std::string;
-using std::pair;
-using std::min;
-
-
-double minimal_distance(vector<int> x, vector<int> y) {
-  //write your code here
-  return 0.;
+ 
+struct Point {
+	int x, y;
+ 
+	double Distance(const Point &p) {
+		auto dx = this->x - p.x, dy = this->y - p.y;
+		return std::sqrt(dx * dx + dy * dy);
+	}
+};
+ 
+typedef std::vector<Point> Points;
+ 
+double closest_strip(Points strip, double d = std::numeric_limits<double>::max()) {
+	auto min = d;
+ 
+	for (auto i = strip.begin(); i != strip.end(); ++i)
+		for (auto j = i + 1; j != strip.end() && j->y - i->y < min; ++j)
+			min = std::min(min, i->Distance(*j));
+ 
+	return min;
 }
-
+ 
+double closest_sorted(Points px, Points py) {
+	if (px.size() <= 3)
+		return closest_strip(px);
+ 
+	auto mid = px.size() / 2;
+	std::vector<Point> pyl, pyr;
+ 
+	for (auto p : px) {
+		if (p.x <= px[mid].x) pyl.push_back(p);
+		else pyr.push_back(p);
+	}
+ 
+	Points pxl = Points(px.begin(), px.begin() + mid);
+	Points pxr = Points(px.begin() + mid + 1, px.end());
+ 
+	auto dl = closest_sorted(pxl, pyl);
+	auto dr = closest_sorted(pxr, pyr);
+	auto dlr = std::min(dl, dr);
+ 
+	Points strip;
+	for (auto p : pyr) 
+		if (p.x - px[mid].x < dlr) strip.push_back(p);
+ 
+	return std::min(dlr, closest_strip(strip, dlr));
+}
+ 
+double closest(Points p) {
+	Points px = p, py = p;
+ 
+	std::sort(px.begin(), px.end(), [](Point p1, Point p2) { return p1.x < p2.x; });
+	std::sort(py.begin(), py.end(), [](Point p1, Point p2) { return p1.y < p2.y; });
+ 
+	return closest_sorted(px, py);
+}
+ 
 int main() {
-  size_t n;
-  std::cin >> n;
-  vector<int> x(n);
-  vector<int> y(n);
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> x[i] >> y[i];
-  }
-  std::cout << std::fixed;
-  std::cout << std::setprecision(9) << minimal_distance(x, y) << "\n";
+	int  n;
+	cin>>n;
+	Points points(n);
+	for(int i=0;i<n;i++)
+	cin>>points[i].x>>points[i].y;
+	std::cout <<setprecision(9)<< closest(points);
+ 
+	return 0;
 }
--- AlgorithmicToolBoxBniversityOfCalifornia/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,12 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+
+int get_change(int m,int x[]) {
+ 
+ int dp[m+1];
+ dp[0]=0;
+ memset(dp,0,sizeof(dp));
+ for(int i=1;i<=m;i++)
+     dp[i] = 1+min(dp[i-x[0]],min(dp[i-x[1]],dp[i-x[2]]));
+     
+ return dp[m];    
 }
 
 int main() {
   int m;
+  int c[]={1,3,4};
   std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  std::cout << get_change(m,c) << '\n';
 }
