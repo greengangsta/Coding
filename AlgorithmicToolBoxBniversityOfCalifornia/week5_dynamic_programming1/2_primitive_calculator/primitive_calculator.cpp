fa39f570ad2be0055f52347a82109044f862bc27
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-01-02 20:22:33 +0530

    final commit successfully solved.

--- AlgorithmicToolBoxBniversityOfCalifornia/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -1,31 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
 
-using std::vector;
-
-vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
-    }
-  }
-  reverse(sequence.begin(), sequence.end());
-  return sequence;
+vector<int> optimal_sequence(int m) {
+  vector <int> sequence;
+  int x[]={2,3};
+ sequence.push_back(m);
+ int dp[m+1];
+ memset(dp,100001,sizeof(dp));
+  dp[0]=0;
+  dp[1]=0;
+ for(int i=2;i<=m;i++)
+   {
+       int y=100001;
+         y = min(y,dp[i-1]);
+       for(int j=0;j<2;j++)
+       {
+          
+           if(i%x[j]==0)
+           {
+             y = min(y,dp[i/x[j]]);
+           }
+       }
+    dp[i] = 1+y; 
+   }
+   for(int i=m;i>=2;)
+     {
+          if(dp[i]==dp[i/x[0]]+1&&i%x[0]==0)
+          {
+              sequence.push_back(i/x[0]);
+               i/=x[0];
+          }
+          else if(dp[i]==dp[i/x[1]]+1&&i%x[1]==0)
+          {
+              sequence.push_back(i/x[1]);
+               i/=x[1];
+          }
+          else if(dp[i]==dp[i-1]+1)
+          {
+              sequence.push_back(i-1);
+               i--;
+          }
+          
+     }
+     //cout<<dp[m]<<endl;
+    reverse(sequence.begin(), sequence.end());
+	return sequence;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
+  
+
+  int m;
+  std::cin >> m;
+  vector<int> sequence = optimal_sequence(m);
   std::cout << sequence.size() - 1 << std::endl;
   for (size_t i = 0; i < sequence.size(); ++i) {
     std::cout << sequence[i] << " ";
   }
+
 }
