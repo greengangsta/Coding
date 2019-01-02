a560ab47b1967b0fb701073fea0f0ec17c559571
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-01-03 00:19:04 +0530

    longest common subsequence
    finding the longest common subsequence using dynamic programming approach.

--- AlgorithmicToolBoxBniversityOfCalifornia/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -1,11 +1,26 @@
-#include <iostream>
-#include <vector>
-
-using std::vector;
+#include <bits/stdc++.h>
+using namespace std;
 
 int lcs2(vector<int> &a, vector<int> &b) {
   //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+  int m,n;
+  m = a.size();
+  n = b.size();
+  int dp[m+1][n+1];
+  for(int i=0;i<=m;i++)
+    dp[i][0]=0;
+  for(int j=0;j<=n;j++)
+    dp[j][0]0;
+  for(int i=1;i<=m;i++)
+    for(int j=1;j<=n;j++)
+       {
+           if(a[i]==b[j])
+            dp[i][j] = dp[i-1][j-1]+1;
+           else
+            dp[i][j] = min(dp[i-1][j],dp[i][j-1])
+       }
+  
+  return  dp[m][n];//std::min(std::min(a.size(), b.size()), c.size());
 }
 
 int main() {
