d6d9e32e691e9a80b79e3c3eba23a14e4660ec0f
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-01-05 00:18:41 +0530

    vector implementation
    Signal 11 error passed with the help of vector implementation.

--- AlgorithmicToolBoxBniversityOfCalifornia/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,35 +1,36 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 
 using std::vector;
 
 int optimal_weight(int W, const vector<int> &w) {
   //write your code here
   int n=w.size();
-  int dp[n+1][W+1];
+  vector <vector <int> > dp(n+1,vector<int>(W+1,0));
   for(int i=0;i<=W;i++)
-     dp[0][i]=0; 
+     dp.at(0).at(i)=0; 
   for(int i=0;i<=n;i++)
-     dp[i][0]=0;   
+     dp.at(i).at(0)=0;   
      
   for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=W;j++)
         {
             int v;
-            dp[i][j] = dp[i-1][j];
+            dp.at(i).at(j) = dp.at(i-1).at(j);
             if(w[i-1]<=j)
               { 
-                v = dp[i-1][j-w[i-1]] + w[i-1];
-                 if(dp[i][j]<v)
-                   dp[i][j]=v;
+                v = dp.at(i-1).at(j-w[i-1]) + w[i-1];
+                 if(dp.at(i).at(j)<v)
+                   dp.at(i).at(j)=v;
                }
         }
     }
-  return dp[n][W];
+  return dp.at(n).at(W);
 }
 
 int main() {
+   
   int n, W;
   std::cin >> W >> n;
   vector<int> w(n);
@@ -37,4 +38,5 @@ int main() {
     std::cin >> w[i];
   }
   std::cout << optimal_weight(W, w) << '\n';
+    
 }
