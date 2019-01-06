442c72e9b9276c94573df5447867340234d55e94
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-01-06 16:42:07 +0530

    debug remaining to the solution.

--- AlgorithmicToolBoxBniversityOfCalifornia/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/tmp1.cpp
+++ AlgorithmicToolBoxBniversityOfCalifornia/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/tmp1.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long int dp1[14][14],dp2[14][14];
+
 long long eval(long long a, long long b, char op) {
   if (op == '*') {
     return a * b;
@@ -12,18 +14,19 @@ long long eval(long long a, long long b, char op) {
     assert(0);
   }
 }
-vector <int> minandmax(int i,int j,string &exp)
+vector <long long int> minandmax(int i,int j,string exp)
 {
-	vector <int> res(2);
-   int min1 = 32767;
+	vector <long long int> res(2);
+    int min1 = 32767;
    int max1 = -32767;
    for(int k=i;k<=j-1;k++)
    {
+        cout<<"check3"<<endl;
        int a,b,c,d;
-       a = eval(dp2[i][k],dp2[k+1][j],exp[k]);
-       b = eval(dp2[i][k],dp1[k+1][j],exp[k]);
-       c = eval(dp1[i][k],dp2[k+1][j],exp[k]);
-       d = eval(dp1[i][k],dp1[k+1][j],exp[k]);
+       a = eval(dp2[i][k],dp2[k+1][j],exp[k*2 +1]);
+       b = eval(dp2[i][k],dp1[k+1][j],exp[k*2 +1]);
+       c = eval(dp1[i][k],dp2[k+1][j],exp[k*2 +1]);
+       d = eval(dp1[i][k],dp1[k+1][j],exp[k*2 +1]);
 	   min1 = min(min1,min(a,min(b,min(c,d))));
 	   max1 = max(max1,max(a,max(b,max(c,d))));
    }
@@ -32,33 +35,57 @@ vector <int> minandmax(int i,int j,string &exp)
 return res;   
 }
 
-long long get_maximum_value(const string &exp) {
+long long get_maximum_value(const string exp) {
   //write your code here
   int n=exp.length();
-  int dp1[n][n],dp2[n][n];
-  for(int i=0;i<n;i++)
+//  int dp1[n][n],dp2[n][n];
+  for(int i=0;i<=n/2;i++)
     {
-        dp1[i][i]=exp[i];
-        dp2[i][i]=exp[i];
+        cout<<"check1"<<endl;
+        dp1[i][i]=exp[i*2]-'0';
+        dp2[i][i]=exp[i*2]-'0';
     }
-  for(int s=1;i<n;i++)
+  for(int s=1;s<=n/2;s++)
    {
-       for(int i=0;j<n-s;i++)
+       for(int i=1;i<=n/2-s;i++)
       {
+           cout<<"check2"<<endl;
           int j=i+s;
-          vector<int> res;
+          vector<long long int> res(2);
           res = minandmax(i,j,exp);
           dp1[i][j]=res[0];
           dp2[i][j]=res[1];
       }
    }
-   
+  for(int i=0;i<=n/2;i++)
+  {
+      for(int j=0;j<=n/2;j++)
+      {
+          cout<<dp1[i][j]<<" ";
+      }
+      cout<<endl;
+  }
+  cout<<endl<<endl;
+  for(int i=0;i<=n/2;i++)
+  {
+      for(int j=0;j<=n/2;j++)
+      {
+          cout<<dp2[i][j]<<" ";
+      }
+      cout<<endl;
+  }
    
   return dp2[0][n-1];
 }
 
 int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+         cout<<"case : "<<t<<endl;
   string s;
   std::cin >> s;
   std::cout << get_maximum_value(s) << '\n';
+    }
 }
