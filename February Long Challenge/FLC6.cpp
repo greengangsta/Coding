e56aa8d768ecc98d46992708df0ce17836967123
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-02-12 19:18:15 +0530

    guess it right partially commited

--- February Long Challenge/FLC6.cpp
+++ February Long Challenge/FLC6.cpp
@@ -1,29 +1,34 @@
-#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <iostream>
+namespace mp = boost::multiprecision;
 using namespace std;
 
-long long int gcd(long long int a, long long int b) 
+mp::cpp_int power(mp::cpp_int x, mp::cpp_int n) 
+{
+    mp::cpp_int y=1;
+    for(int i=0;i<n;i++)
+    {
+        y*=x;
+    }
+  return y;
+}
+mp::cpp_int modInverse(mp::cpp_int a,mp::cpp_int m) 
 { 
-    if (a == 0) 
-        return b; 
-    return gcd(b % a, a); 
-} 
-long long int modInverse(long long int a, long long int m) 
-{ 
-   long long  int m0 = m; 
-    long long int y = 0, x = 1; 
+   mp::cpp_int m0 = m; 
+    mp::cpp_int y = 0, x = 1; 
     if (m == 1) 
       return 0; 
     while (a > 1) 
     {
-       long long int q = a / m; 
-        long long int t = m; 
-        m = a % m, a = t; 
-        t = y; 
-        y = x - q * y; 
-        x = t; 
+       mp::cpp_int q=a/m; 
+       mp::cpp_int t=m; 
+        m=a%m,a=t; 
+        t=y; 
+        y=x-q*y; 
+        x=t; 
     } 
-    if (x < 0) 
-       x += m0; 
+    if (x<0) 
+       x+=m0; 
     return x; 
 }
 int main() {
@@ -31,33 +36,24 @@ int main() {
     cin>>t;
      while(t--)
       {
-          long long int mod=1000000007;
-          long long int n,k,m;
-          long long int p=0,q=1;
+          unsigned long long int mod=1000000000+7;
+           mp::cpp_int n,k,m,p,q,a,b,c;
           cin>>n>>k>>m;
-         for(long long int i=1;i<=m;i++)
-         {
-             if(m-i>=1&&n>k)
-             {
-                 n=n%k;
-             }
-             else
-             {
-            
-                   long long int a=gcd(q-p,q*n);
-                  // cout<<"a : "<<a<<endl;
-                   long long int x=(q-p)/a;
-                    long long int y=(q*n)/a;
-                    long long int l =(q*y)/gcd(q,y);
-                    p=(p*(l/q)) + (x*(l/y));
-                    q=l;
-                     n+=k;
-                    // cout<<"p/q : "<<p<<'/'<<q<<endl;
-                    // cout<<"n : "<<n<<endl;
-             }
-         }
+          a=n,b=n-1,c=n+k;
+           if(m%2==0)
+            {
+              p=((power(a,m/2)-power(b,m/2))*c +power(b,m/2))%mod;
+              q= modInverse(power(a,m/2)*c,mod)%mod;
+              cout<<(p*q)%mod<<endl;
+            }
+            else
+            {
+              p= (power(a,(m+1)/2) -power(b,(m+1)/2) )%mod;
+              q= modInverse(power(a,(m+1)/2),mod)%mod;
+              cout<<(p*q)%mod<<endl;
+            }
          
-           cout<<p*modInverse(q,mod)<<endl;
+          // cout<<p*modInverse(q,mod)<<endl;
            //cout<<modInverse(207,mod);
       }
            
