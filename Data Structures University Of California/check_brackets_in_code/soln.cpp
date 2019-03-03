0d1ba7f56ea303fbf664bdd14b0fbd4f130fcb96
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-03-03 22:21:32 +0530

    opening brackets case

--- Data Structures University Of California/check_brackets_in_code/soln.cpp
+++ Data Structures University Of California/check_brackets_in_code/soln.cpp
@@ -2,17 +2,13 @@
 using namespace std;
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
      stack <char> x;
      string s;
      cin>>s;
      for(int i=0;i<s.length();i++)
     {
         if(s[i]=='('||s[i]=='{'||s[i]=='[')
-         x.push(s[i]);
+          x.push(s[i]);
         else if(s[i]==')'||s[i]=='}'||s[i]==']')
         {
             if(s[i]==')'&&x.top()!='(')
@@ -33,12 +29,13 @@ int main()
             else
              x.pop();
         }
-          else if(i==s.length()-1&&x.empty())
+         if(i==s.length()-1&&x.empty())
              cout<<"Success"<<endl;
+         else if(i==s.length()-1&&!x.empty())
+             cout<<i+1<<endl;
+      }
     
-    }
-    
-    }
+
     
     
     return 0;
