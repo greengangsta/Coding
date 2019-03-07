9622c3745f5245582a52b68cd08301347b6ac60f
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-03-07 22:52:12 +0530

    network packet simulation successfully solved

--- Data Structures University Of California/network_packet_processing_simulation/soln.cpp
+++ Data Structures University Of California/network_packet_processing_simulation/soln.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 int main()
 {
+    
+
+    
+    
    int s,n;
    cin>>s>>n;
    int t=s;
-   
    deque <int> fin_time;
    for(int i=0;i<n;i++)
     {
         int a,p;
         cin>>a>>p;
-        if(fin_time.empty())
+        if(fin_time.empty()&&t>0)
         {
           cout<<a<<endl;
           fin_time.push_back(a+p);
@@ -20,20 +23,28 @@ int main()
         }
         else if(!fin_time.empty()&&t>0)
         {
-            cout<<fin_time.back();
-            fin_time.push_back(a+p);
+            cout<<max(a,fin_time.back())<<endl;
+            fin_time.push_back(max(a,fin_time.back())+p);
+            t--;
         }
         else
         {
-            while(fin_time.front()<a+p)
+            while(fin_time.front()<=a&&!fin_time.empty())
             {
                 fin_time.pop_front();
                 t++;
             }
-            if(t>0)
+            if(t>0&&fin_time.empty())
             {
-                cout<<fin_time.back();
+                cout<<a<<endl;
                  fin_time.push_back(a+p);
+                 t--;
+            }
+            else if(t>0&&!fin_time.empty())
+            {
+             cout<<max(a,fin_time.back())<<endl;
+             fin_time.push_back(max(a,fin_time.back())+p);
+             t--;
             }
             else
             {
@@ -42,6 +53,8 @@ int main()
         }
     }
     
+   
+
     
     
     return 0;
