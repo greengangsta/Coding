3d99e892b4d2562918d5012c0fe0e57de4ab1875
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-03-07 11:24:34 +0530

    used two stacks
    stack y for storing the index of the brackets.
    Successfully solved and submitted

--- Data Structures University Of California/check_brackets_in_code/soln.cpp
+++ Data Structures University Of California/check_brackets_in_code/soln.cpp
@@ -3,12 +3,16 @@ using namespace std;
 int main()
 {
      stack <char> x;
+     stack <int> y;
      string s;
      cin>>s;
      for(int i=0;i<s.length();i++)
-    {
+      {
         if(s[i]=='('||s[i]=='{'||s[i]=='[')
-          x.push(s[i]);
+         { 
+             x.push(s[i]);
+             y.push(i+1);
+         }
         else if(s[i]==')'||s[i]=='}'||s[i]==']')
         {
             if(s[i]==')'&&(x.empty()||x.top()!='('))
@@ -16,23 +20,25 @@ int main()
                 cout<<i+1<<endl;
                 break;
             }
-            else if(s[i]=='['&&(x.empty()||x.top()!=']'))
+            else if(s[i]==']'&&(x.empty()||x.top()!='['))
             {
                 cout<<i+1<<endl;
                 break;
             }
-            else if(s[i]=='{'&&(x.empty()||x.top()!='}'))
+            else if(s[i]=='}'&&(x.empty()||x.top()!='{'))
             {
                 cout<<i+1<<endl;
                 break;
             }
             else
-             x.pop();
+            { x.pop();
+              y.pop();
+            }
         }
-         if(i==s.length()-1&&x.empty())
+        if(i==s.length()-1&&x.empty())
              cout<<"Success"<<endl;
-         else if(i==s.length()-1&&!x.empty())
-             cout<<i+1<<endl;
+        else if(i==s.length()-1&&!x.empty())
+             cout<<y.top()<<endl;
       }
     
 
