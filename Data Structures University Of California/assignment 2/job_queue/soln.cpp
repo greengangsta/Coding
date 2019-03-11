0946d8d11131dbd1555d218091a8e9332dcb76cf
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-03-12 00:07:05 +0530

    added insertion and extraction function

--- Data Structures University Of California/assignment 2/job_queue/soln.cpp
+++ Data Structures University Of California/assignment 2/job_queue/soln.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 struct node
 {
-    int p;
-    int k;
+    long long int p;
+    long long int k;
 };
 void min_heap( node pr[], long long int  j, long long int n)
 {
@@ -25,38 +25,61 @@ void min_heap( node pr[], long long int  j, long long int n)
                  min_heap(pr,min,n);
             }
 }
-node extract_min(node pr[])
+
+void siftup(node pr[],long long int j,long long int n)
 {
-    
+    while(pr[j/2].k<pr[j].k||(pr[j/2].k==pr[j].k&&pr[j/2].p<pr[j].p))
+    {
+        swap(pr[j/2],pr[j]);
+    }
+}
+node extract_min(node pr[],long long int n)
+{
+    long long int i=n-1;
+    node a = pr[0];
+    swap(pr[i],pr[0]);
+    min_heap(pr,0, n);
+    n = n-1;
+    return a;
 }
 
-void insert(node pr[])
+void insert(node pr[],long long int n,node a)
 {
-    
+  pr[n-1] = a;
+  siftup(pr,n-1,n);
 }
 int main()
 {
      
-    int n,m;
+    long long int n,m;
     cin>>n>>m;
-    int a[m];
+    long long int x[m];
     node pr[n];
-    for(int i=0;i<m;i++)
-      cin>>a[i];
-    for(int i=0;i<n;i++)
+    for(long long int i=0;i<m;i++)
+      cin>>x[i];
+       for(long long int i=0;i<n;i++)
+     {
+         cout<<i<<" ";
+         cout<<0;
+         cout<<endl;
+     }
+    for(long long int i=0;i<n;i++)
      {
          pr[i].p=i;
-         pr[i].k=0;
-     }
-     for(int i=0;i<n;i++)
-     {
-         cout<<pr[i].p<<" ";
-         cout<<pr[i].k;
-         cout<<endl;
+         pr[i].k=x[i];
      }
      for( long long int  j = n/2;j>=0;j--)
        min_heap(pr,j,n);
-     
+     for(long long int i=n;i<m;i++)
+      {
+          node a,b;
+           a = extract_min(pr, n);
+           b.p = a.p;
+           b.k = a.k + x[i];
+          insert(pr,n,b);
+          cout<<a.p<<" ";
+          cout<<a.k<<endl;
+      }
      
     
     
