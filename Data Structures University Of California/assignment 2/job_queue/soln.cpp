5038deb39f8e1d15a81bc0f90b2b8af26486b4b2
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-03-13 00:32:29 +0530

    finally successfully solved integer overflow was occuring

--- Data Structures University Of California/assignment 2/job_queue/soln.cpp
+++ Data Structures University Of California/assignment 2/job_queue/soln.cpp
@@ -24,24 +24,24 @@ void min_heap( node pr[], long long int  j, long long int n)
            node m1,m2,m3;
            if(l<n)
            m1 = pr[l];
+           else
+           {
+           	m1.p=n+1;
+           	m1.k = 99999999999999999;
+		   }
            if(r<n)
            m2 =pr[r];
+           else
+           {
+           	m2.p =n+1;
+           	m2.k = 999999999999999999;
+		   }
            m3 = min1(m1,min1(m2,pr[j]));
            if(m3.p==m1.p)
            min = l;
            else if(m3.p==m2.p)
            min=r;
-           
-           /*
-          if(l<n && pr[l].k < pr[j].k)
-               min = l;
-          if(l<n && pr[l].k == pr[j].k && pr[l].p < pr[j].p)
-               min = l;
-          if(r<n&& pr[r].k<pr[j].k&&pr[r].k<pr[l].k)
-               min = r;
-          if(r<n&&pr[r].k<pr[l].k&&pr[r].k==pr[j].k&&pr[r].p<pr[j].p)
-               min = r;
-                 */
+
           if(min!=j)
             {
                  swap(pr[j],pr[min]);
@@ -52,9 +52,10 @@ void min_heap( node pr[], long long int  j, long long int n)
 
 void siftup(node pr[],long long int j,long long int n)
 {
-    while(pr[j].k<pr[(j-1)/2].k||(pr[j].k==pr[(j-1)/2].k&&pr[j].p<pr[(j-1)/2].p))
+    while(j>0&&(pr[j].k<pr[(j-1)/2].k||(pr[j].k==pr[(j-1)/2].k&&pr[j].p<pr[(j-1)/2].p)))
     {
         swap(pr[(j-1)/2],pr[j]);
+        j = (j-1)/2;
     }
 }
 node extract_min(node pr[],long long int n)
@@ -77,20 +78,21 @@ int main()
     long long int n,m;
     cin>>n>>m;
     long long int x[m];
-    node pr[n];
+     node pr[n];
     for(long long int i=0;i<m;i++)
       cin>>x[i];
-    for(long long int i=0;i<n;i++)
+    for(long long int i=0;i<n&&i<m;i++)
      {
          cout<<i<<" ";
          cout<<0;
          cout<<endl;
      }
-    for(long long int i=0;i<n;i++)
+    for(long long int i=0;i<n&&i<m;i++)
      {
          pr[i].p=i;
          pr[i].k=x[i];
      }
+     n = min(n,m);
      for(long long int  j = n/2;j>=0;j--)
        min_heap(pr,j,n);
      for(long long int i=n;i<m;i++)
@@ -98,7 +100,7 @@ int main()
           node a,b;
            a = extract_min(pr, n);
            b.p = a.p;
-           b.k = a.k+x[i] ;
+           b.k = a.k+x[i];
           insert(pr,n,b);
           cout<<a.p<<" ";
           cout<<a.k<<endl;
