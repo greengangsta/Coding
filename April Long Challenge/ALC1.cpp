d9cb88f2ebbabe9f294b8a6fc0f8b4d9b64f3809
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-04-05 20:17:39 +0530

    updated
    fully solved

--- April Long Challenge/ALC1.cpp
+++ April Long Challenge/ALC1.cpp
@@ -5,11 +5,15 @@ int main()
 {
    int n;
    cin>>n;
-   int a[n];
+   long int a[n];
    for(int i=0;i<n;i++)
      cin>>a[i];
    sort(a,a+n);
-   cout<<a[n-2]%a[n-1]<<endl;
+   long int b[n-1];
+   for(int i=0;i<n-1;i++)
+     b[i] = a[i]%a[n-1];
+   sort(b,b+n);
+   cout<<b[n-2]<<endl;
     
     
     return 0;
