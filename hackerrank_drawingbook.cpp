748fa490cc6652ae16a5bd24c40874b0eddf96fc
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-05-10 11:34:54 +0530

    hackerrank drawing book initial commit
    26/27 cases passed

---
+++ hackerrank_drawingbook.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+int n,p;
+  cin>>n;
+  cin>>p;
+  int front=1,back=n;
+  int c1=0,c2=0;
+  for(int i=1;i<p;i++)
+  {
+      if(front==p||front==p+1)
+        break;
+      front+=2;
+      c1++;
+  }
+  for(int i=n;i>=p-1;i--)
+  {
+      if(back==p||back==p+1)
+        break;
+       back-=2;
+       c2++;
+  }
+  cout<<min(c1,c2)<<endl;
+
+
+
+
+
+    return 0;
+}
