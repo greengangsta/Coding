3c8a98c202a5ad539f1fae855bb890c095f6aa54
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-06-06 10:38:18 +0530

    added solution code for hackerrank problem max xor

---
+++ maxxor_hackerrank.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int l,r;
+    cin>>l>>r;
+    int max = 0;
+    for(int i=l;i<=r;i++)
+      for(int j=i;j<=r;j++)
+        {
+            if((i^j)>max)
+             max = i^j;
+        }
+     cout<<max<<endl;   
+    return 0;
+}
