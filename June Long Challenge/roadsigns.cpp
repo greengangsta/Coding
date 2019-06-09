ed7d0d777823ac89efde05a757793423224b46c1
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-06-09 14:15:05 +0530

    road signs subtask 2 complete

--- June Long Challenge/roadsigns.cpp
+++ June Long Challenge/roadsigns.cpp
@@ -1,57 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int cnt(int k1,int k2)
+long long int  fastpower(long long int  a,long long int  i)
 {
-	int dig[10]={0};
-	do
-	{
-		int r =k1%10;
-		dig[r] =1;
-		k1/=10;
-	}while(k1);
-	do
-	{
-		int r =k2%10;
-		dig[r] =1;
-		k2/=10;
-	}while(k2);
-	int count=0;
-	for(int i=0;i<10;i++)
-	 if(dig[i]==1)
-	  count++;
-
- return count;
+    long long int  mod=1000000007; 
+    long long int b=1;
+     if(i==0)
+      return 1;
+     else if(i==1)
+		return a;
+	 else if(i%2==0)
+		return ((fastpower(a,i/2)%mod)*(fastpower(a,i/2)%mod))%mod;
+	 else
+	    return ((a%mod)*((fastpower(a,i/2)%mod)*(fastpower(a,i/2)%mod))%mod)%mod;
+		 
 }
 
 int main()
 {
+	long long int mod = 1000000000 + 7;	
 	int t;
 	cin>>t;
 	while(t--)
 	{
-	   int k;
+	   long long int k;
 	   cin>>k;
-	   int n;
-	   n = pow(10,k);
-	   int a[n],b[n];
-	   for(int i=0;i<n;i++)
-	    {
-	    	a[i] = i;
-	    	b[i] = n-i-1;
-		}
-		int count = 0;
-		for(int i=0;i<n;i++)
-		{
-			int c1,c2;
-			c1 = cnt(a[i],b[i]);
-			if(c1==2)
-			{
-		//	cout<<a[i]<<": "<<b[i]<<" | ";
-			count++;
-	     	}
-		}
-		cout<<count<<endl;
+	   int ans;
+	   ans  = 10;
+	   long long int fact;
+	   fact = fastpower(2,k-1);
+	   ans = ((ans%mod)*(fact%mod))%mod;
+	   cout<<ans<<endl;
+	   
     }
     
     
