e760b3ae6a606876d2ea971d5d48eef59ea9a790
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-06-11 00:18:49 +0530

    chef n ingredients subtask 1

--- June Long Challenge/chefningredients.cpp
+++ June Long Challenge/chefningredients.cpp
@@ -4,27 +4,15 @@ using namespace std;
 int main()
 {
 	
-	
 	int t;
 	cin>>t;
 	while(t--)
 	{
-		unsigned long long int n,k,mod,c11,c12;
-		mod = 1000000007;
-		cin>>n>>k;
-        
-		if(n+k-1>=(2*k)-1)
-		cout<<k-1<<endl;
-		else
-		{
-			unsigned long long int sum=0;
-			sum+= k-1;
-			k = k-n;
-			k = ((k%mod)*((k+1)%mod))%mod;
-			sum+=k/2;
-			cout<<sum<<endl;
-			
-		}
+		unsigned long long int n,k,mod;
+		mod = 1000000000+7;
+		  cin>>n>>k;	   
+		 cout<<((k%mod)*((k-1)%mod))/2)%mod<<endl;
+		
 	}
 
     
