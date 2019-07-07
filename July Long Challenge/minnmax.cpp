33f3fae4f51ba8eda0bf25eb8c59b484bc9331ff
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-07-07 10:30:45 +0530

    july long challenge second problem initial commit

---
+++ July Long Challenge/minnmax.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       int n,k;
+       cin>>n;
+       cin>>k;
+       int c ;
+       c = k%n;
+       int a[n];
+       cout<<c<<endl;
+    }
+    
+    
+    
+    return 0;
+}
