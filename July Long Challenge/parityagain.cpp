74569d07b296b8ef23240694f114599136c9412b
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-07-10 17:11:14 +0530

    parity again fully solved code

--- July Long Challenge/parityagain.cpp
+++ July Long Challenge/parityagain.cpp
@@ -1,19 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/*
-int parity(int n)
-{
-    if(n==0)
-     return 0;
-    else if (n%2==0)
-     return parity(n/2);
-    else
-     return  1 - parity(n/2);
-     
-}
-*/
-
 int main()
 {
     int t;
@@ -23,28 +10,32 @@ int main()
         int q,x;
         unordered_set <int> s;
         cin>>q;
+        int c1 = 0,c2 = 0;
         for(int i=0;i<q;i++)
         {
             cin>>x;
-            int c1 = 0,c2 = 0;
-            s.insert(x);
             vector <int> v1;
+            v1.push_back(x);
+            if(s.find(x)==s.end())
+            {
+                s.insert(x);
             for(auto it = s.begin();it!= s.end();it++)
             {
                     if(*it != x)
                      v1.push_back(*it ^ x);
-                   // s.insert(*it ^ x);
             }
             for(auto it = v1.begin();it!=v1.end();it++)
-              s.insert(*it);
-             for(auto it = s.begin();it!= s.end();it++)
-            {
-                bitset <32> b1(*it); 
+             {
+                s.insert(*it);
+               bitset <32> b1(*it); 
                  if(b1.count()%2==0)
                      c1 ++;
                     else
-                     c2 ++;
+                     c2 ++;  
+             }
+             s.insert(x);
             }
+             v1.clear();
             cout<<c1<<" "<<c2<<endl;
         }
     }
