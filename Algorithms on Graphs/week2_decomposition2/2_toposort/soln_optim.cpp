56c474b48e4fcee998e8aa60f9e21bd3ba38ee3b
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-08-27 16:49:52 +0530

    finally solved the problem
    stupid mistake of passing the data structure instead of a reference to the data structure

--- Algorithms on Graphs/week2_decomposition2/2_toposort/soln_optim.cpp
+++ Algorithms on Graphs/week2_decomposition2/2_toposort/soln_optim.cpp
@@ -12,58 +12,53 @@ bool cmp(const node n1, const node n2)
 {
     return n1.postorder > n2.postorder;
 }
-vector<int> reachable(vector <vector<int>> adj,int u,vector <int> visited,vector<int> & post_order)
-{
-    
+void reachable(vector <vector<int> > &adj,int u,vector <int> &visited,vector<int> &post_order)
+{ 
     visited[u] = 1;
     all_visited += 1;
     for(int i = 0;i<adj[u].size();i++)
     {
         if(visited[adj[u][i]]==0)
-          visited = reachable(adj,adj[u][i],visited,post_order);
+         reachable(adj,adj[u][i],visited,post_order);
     }
     post_order[u] = k;
     k += 1;
-    return visited;
+    return ;
 }
 
 
-vector<int> top_sort(vector <vector<int>> adj,vector <int> visited)
+void top_sort(vector< vector<int> > &adj,vector <int> &visited,vector<int> &post_order)
 {
-    vector<int> post_order(adj.size());
     for(int i = 0;i<adj.size();i++)
     {
-         if(all_visited==adj.size())
+        if(visited[i]==0)
+         {
+            reachable(adj,i,visited,post_order);
+         }
+        else if(all_visited==adj.size())
         {
-          //  cout<<i<<endl;
             break;
         }
-		else if(visited[i]==0)
-         {
-           visited = reachable(adj,i,visited,post_order);
-         }
-       
     }
-   
-    return post_order;
+    return ;
 }
 
 int main()
 {
    int n,m;
    cin>>n>>m;
-   vector <vector<int>> adj(n);
+   vector < vector<int> > adj(n);
    for(int i=0;i<m;i++)
    {
-      int u,v;
-      cin >> u>>v;
+     int u,v;
+     cin >> u>>v;
       adj[u-1].push_back(v-1);
    }
    vector<int> visited(n);
    for(int i = 0;i<n;i++)
      visited.push_back(0);
    vector<int> post_order(n);
-   post_order = top_sort(adj,visited);
+   top_sort(adj,visited,post_order);
    node order[n];
    for(int i=0;i<n;i++)
    {
