8c4e61093ca4d891c0f5e197e81923223b9e04d7
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-08-31 10:35:32 +0530

    max element in stack optimized all test cases solved solution

--- max_element_in_stack.cpp
+++ max_element_in_stack.cpp
@@ -3,58 +3,43 @@ using namespace std;
 
 int main()
 {
+
+   stack <int> s,m;
    int n;
    cin>>n;
-   int max = 0;
-   set <int> s;
-   stack <int> st;
-   unordered_map <int,int> mp;;
-   for(int i=0;i<n;i++)
+    m.push(-1);
+   while(n--)
    {
        int a;
-       cin>>a; 
+       cin>>a;
        if(a==1)
        {
-           int x;
-           cin>>x;
-           if(x>max)
-            max = x;
-           st.push(x);
-           s.insert(x);
-           if(mp.find(x)==mp.end())
-             {
-                 mp.insert({x,1});
-             }
-            else{
-                auto it = mp.find(x);
-                it->second += 1;
-            }
+         int x;
+         cin>>x;
+         s.push(x);
+         if(x>=m.top())
+          m.push(x);
        }
        else if(a==2)
        {
-           int x;
-           x = st.top();
-           if(max==x)
-           {
-            auto it = mp.find(x);
-            if(it->second==1||it->second==0)
-            {
-             s.erase(it->second);
-             it->second = 0;
-             max = *s.end();
-            }
-            else
-            {
-                it->second -= 1;
-            }
-           }
-           st.pop();
+           int x = m.top();
+           int y = s.top();
+          if(x==y)
+             {
+                 s.pop();
+                 m.pop();
+             }
+           else
+             s.pop();
        }
        else
        {
-           cout<<max<<endl;
+          cout<<m.top()<<endl;
        }
    }
+
+
+
 
 
 
