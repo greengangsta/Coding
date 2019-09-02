98c0006584e6d34185246f9587a2a37c3ed538d6
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-09-02 11:48:48 +0530

    hackerrank problem larrys array all test cases passed

--- hackerrank_larrys_array.cpp
+++ hackerrank_larrys_array.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int is_sorted(int a[],int l,int h)
+{
+  for(int i=l;i<h-1;i++)
+   {
+       if(a[i]>a[i+1])
+        return 0;
+   }
+   return 1;
+}
+
 int main()
 {
    int t;
@@ -22,12 +32,14 @@ int main()
         for(int i = N;i<n;i++)
             a[i] = i+1;
         int inv = 0;
-        for(int i=0;i<n;i++)
-         for(int j=i+1;j<n;j++)
-         {
-              if(a[i]>a[j])
-               inv++;
-         }
+        multiset<int> set1; 
+        set1.insert(a[0]);
+        for(int i=1;i<n;i++) 
+        { 
+            set1.insert(a[i]);
+            auto it = set1.upper_bound(a[i]); 
+            inv += distance(it, set1.end()); 
+        } 
         if(inv%2==0)
          cout<<"YES"<<endl;
         else 
