1424eba00267af0ebb7872361eb116fa6c798cde
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-09-05 12:48:44 +0530

    construct the array all test cases passed

--- hackerrank_DP_constructdarray.cpp
+++ hackerrank_DP_constructdarray.cpp
@@ -5,23 +5,26 @@ int main()
 {
   int n,k,x;
   cin>>n>>k>>x;
-  long long int dp[n][k];
-  long long int mod  = 10e9 + 7;
+  long long int dp[n][2];
+  long long int mod  = 1000000000 + 7;
   dp[0][0] = 1;
-  for(int j = 1;j<n;j++)
-   dp[0][j] = 0;
+  dp[0][1] = 0;
   for(int i=1;i<n;i++)
    {
        dp[i][0] = ((((k-1)%mod)*(dp[i-1][1]%mod))%mod); 
-       dp[i][1] =  dp[i-1][0] +  ((((k-2)%mod)*(dp[i-1][1]%mod))%mod); 
+       dp[i][1] =  ((dp[i-1][0]%mod)+  ((((k-2)%mod)*(dp[i-1][1]%mod))%mod))%mod; 
    }
-  if(x==1)
+    if(x==1)
    cout<<dp[n-1][0];
   else
    cout<<dp[n-1][1];
-
-
-
+   /*
+   cout<<endl;
+   for(int i=0;i<n;i++)
+   {
+       cout<<dp[i][0]<<" "<<dp[i][1]<<endl;
+   }
+  */
 
 
     return 0;
