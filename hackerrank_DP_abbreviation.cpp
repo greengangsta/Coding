c7e013c682032b930e96ec2c38f74c0e34ec461f
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-09-06 16:00:29 +0530

    abbreviation problem all test cases passed

--- hackerrank_DP_abbreviation.cpp
+++ hackerrank_DP_abbreviation.cpp
@@ -9,60 +9,62 @@ int main()
    {
        string s1,s2;
        cin>>s1>>s2;
-       int n = s1.length();
-       int a[n] = {0};
-       int j = 0;
-       int a1[26] = {0},b1[26] = {0};
-       for(int i=0;i<s1.length();i++)
+       int n,m;
+       n = s1.length();
+       m  = s2.length();
+       bool dp[n+1][m+1];
+       dp[0][0] = true;
+       for(int j = 1;j<=m;j++)
+        dp[0][j] = false;
+        int count = 0;
+       for(int i=1;i<=n;i++)
        {
-          if(isupper(s1[i]))
-           a1[s1[i]-'A'] += 1;
-       }
-       for(int i=0;i<s2.length();i++)
-       {
-         if(isupper(s2[i]))
-           b1[s2[i]-'A'] += 1;
-       }
-       for(int i=0;i<n;i++)
-       {
-           char c = toupper(s1[i]);
-           if(s1[i]==s2[j])
+           if(s1[i-1]>=65 && s1[i-1]<=90||count == 1)
            {
-               a[i] = a[i-1]+1;
-               j++;
-           }
-           else if(c ==s2[j])
-           {
-               j++;
-               if(a[i-1]>0)
-                a[i] = a[i-1] +1;
-               else 
-                a[i] = 1;
+             dp[i][0] = false;
+             count = 1;
            }
            else
-            a[i] = a[i-1];
-       }
-       int flag = 0;
-       for(int i=0;i<n;i++)
-       {
-           if(a[i]==s2.length())
            {
-               flag = 1;
+              dp[i][0] = true;
+           } 
+       }
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=m;j++)
+           {
+               if(s1[i-1]==s2[j-1])
+               {
+                   dp[i][j] = dp[i-1][j-1];
+               }
+               else if(toupper(s1[i-1])==s2[j-1])
+               {
+                   dp[i][j] = dp[i-1][j-1] || dp[i-1][j];
+               }
+               else if(s1[i-1]>=65 && s1[i-1]<=90)
+               {
+                   dp[i][j] = false;
+               }
+               else
+               {
+                   dp[i][j] = dp[i-1][j];
+               }
+
            }
        }
-       if(s1.length()<s2.length())
-        flag = 0;
-       for(int i=0;i<26;i++)
+       dp[n][m]?cout<<"YES":cout<<"NO";
+       cout<<endl;
+
+    /*
+       for(int i=0;i<=n;i++)
        {
-           if(a1[i]>b1[i])
-            flag = 0;
+           for(int j=0;j<=m;j++)
+           {
+               cout<<dp[i][j]<<" ";
+           }
+           cout<<endl;
        }
-       if(flag==1)
-        cout<<"YES"<<endl;
-       else
-        cout<<"NO"<<endl;
-      // for(int i=0;i<n;i++)
-       // cout<<a[i]<<" ";
+    */
    }
 
 
