41b0b9a9394cc79a86bbe5006784497e87f528e2
Suraj Tripathi <36106756+greengangsta@users.noreply.github.com>
2024-02-01 21:50:56 +0530

    Create topViewTree_GFG.cpp

---
+++ codehelp/topViewTree_GFG.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        //Your code here
+         map <int, int> mp; 
+        
+        queue<pair<Node*, int> > q; 
+        q.push(make_pair(root, 0)); 
+        while(!q.empty())
+        {
+            pair<Node*, int> p = q.front(); 
+            q.pop();
+            int distance = p.second; 
+            if(mp.find(distance) == mp.end())
+            mp[distance] = p.first->data;
+            if(p.first->left) 
+            q.push(make_pair(p.first->left, distance -1)); 
+            if(p.first->right) 
+            q.push(make_pair(p.first->right, distance + 1)); 
+            
+            
+        }
+        
+        vector <int> ans; 
+        for(auto it: mp){
+            // cout<<it.first<<" ";
+                ans.push_back(it.second);
+        }
+        return ans;
+    }
+
+};
