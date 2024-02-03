e07a514ee28ba1551944288005b70ed6a56ab764
Suraj Tripathi <36106756+greengangsta@users.noreply.github.com>
2024-02-03 21:19:43 +0530

    Create searchInBST_CNCS.cpp

---
+++ codehelp/searchInBST_CNCS.cpp
@@ -0,0 +1,19 @@
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+
+
+  if (root == NULL) {
+      return false; 
+  }
+
+    
+  if (root->data == x) {
+      return true; 
+  }
+
+  else if(root->data < x)
+    return searchInBST(root->right, x); 
+
+  else 
+    return searchInBST(root->left, x); 
+
+}
