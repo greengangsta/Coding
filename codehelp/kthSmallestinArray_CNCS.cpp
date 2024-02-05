130b8f3351fb26862a4fa10359ab89402bec9de7
Suraj Tripathi <36106756+greengangsta@users.noreply.github.com>
2024-02-05 14:35:19 +0530

    Create kthSmallestinArray_CNCS.cpp

---
+++ codehelp/kthSmallestinArray_CNCS.cpp
@@ -0,0 +1,27 @@
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        priority_queue <int> pq; 
+        for(int i=l;i<l+k;i++)
+        {
+            pq.push(arr[i]);
+        }
+        
+        for(int i=l+k;i<=r;i++){
+            int top = pq.top(); 
+            if(arr[i]< top)
+            {
+              pq.pop();
+              pq.push(arr[i]);
+            }
+
+        }
+        
+        return pq.top(); 
+    }
+};
