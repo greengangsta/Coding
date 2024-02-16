97cb9a1cae52c0900affdc9b14a2990226962c0c
Suraj Tripathi <36106756+greengangsta@users.noreply.github.com>
2024-02-16 23:18:44 +0530

    Update minSwapToIncreasingArrays_LC.cpp

--- codehelp/dp/minSwapToIncreasingArrays_LC.cpp
+++ codehelp/dp/minSwapToIncreasingArrays_LC.cpp
@@ -19,10 +19,17 @@ public:
             if(swapped){
                 swap(prev1, prev2); 
             }
+    
+           // no swap case ==>> agar pahle se increasing order hai to bina swap kiye kaam chal sakta hai 
+           // meaning ki no swap ki call aap tabhi kar sakte ho jab pahle se increasing order ho
            
             if(nums1[index] > prev1 && nums2[index] > prev2){
                 ans = solve(nums1, nums2, index+1, 0, dp); 
             }
+
+          // swap case ==>> swap karne se pahle ye check karna hai ki swapping ke baad increasing order rahega ki nahi 
+          // IMP_NOTE ==> agar increasing order pahle se hai to bhi ye function call hogi kyuki ye if statement sirf ye 
+          // check karta hai ki swap karne ke baad increasing order rahegi ya nahi. 
 
             if(nums1[index] > prev2 && nums2[index] > prev1){
                 ans = min (ans, 1+ solve(nums1, nums2, index+1, 1, dp)); 
