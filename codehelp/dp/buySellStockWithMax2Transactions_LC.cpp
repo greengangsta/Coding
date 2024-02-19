71b0493e2794a02469387de3e97d169d9a3c0723
Suraj Tripathi <36106756+greengangsta@users.noreply.github.com>
2024-02-19 17:20:34 +0530

    Create buySellStockWithMax2Transactions_LC.cpp

---
+++ codehelp/dp/buySellStockWithMax2Transactions_LC.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  private : 
+     int solve(vector<int>& prices, int index, bool buy, int count,  vector <vector<vector<int>>> &dp ){
+        if(index >= prices.size() || count ==0){
+            return 0; 
+        }
+
+        if(dp[index][buy][count] != - 1){
+            return dp[index][buy][count]; 
+        }
+
+        int ans = 0; 
+        // case buy
+        if(buy){
+            ans = max(solve(prices, index + 1 , false, count, dp ) - prices[index], solve(prices, index + 1 , true, count, dp )); 
+        }
+        else{
+            ans = max(solve(prices, index + 1 , true, count - 1, dp ) + prices[index], solve(prices, index + 1 , false, count, dp )); 
+        }
+
+        return dp[index][buy][count] = ans; 
+        // return dp[index][buy] =  ans; 
+     }
+
+public:
+    int maxProfit(vector<int>& prices) {
+        
+         int n = prices.size(); 
+        vector <vector<vector<int>>> dp (n+1, vector<vector<int>>(2, vector<int>(3, -1))); 
+        return solve(prices, 0, true, 2, dp ); 
+    }
+};
