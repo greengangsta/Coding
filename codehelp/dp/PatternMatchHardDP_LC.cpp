1b56df543adb90f755ced88299b871248dcc47ce
Suraj Tripathi <36106756+greengangsta@users.noreply.github.com>
2024-02-19 22:55:02 +0530

    Create PatternMatchHardDP_LC.cpp

---
+++ codehelp/dp/PatternMatchHardDP_LC.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private : 
+    bool solve(string &s, string &p, int i, int j,  vector <vector<int>> &dp){
+
+        if(j<0 && i<0){
+            return true; 
+        }
+
+        if(i>=0 && j<0 ){
+            return false; 
+        }
+
+        if(i<0 && j>=0 ){
+           for(int k=0;k<=j;k++){
+               if(p[k]!= '*'){
+                   return false; 
+               }
+           }
+           return true; 
+        }
+
+        if(dp[i][j]!= -1){
+            return dp[i][j]; 
+        }
+
+        if(s[i] == p[j] || p[j]== '?'){
+            return dp[i][j] = solve(s, p, i-1, j-1, dp); 
+        }
+        else if(p[j] == '*')
+        {
+            return dp[i][j] = solve(s, p, i-1, j, dp) || solve(s, p, i, j-1, dp); 
+        }
+        else {
+            return dp[i][j] = false; 
+        }
+
+    }
+public:
+    bool isMatch(string s, string p) {
+          int n = s.length(); 
+      int m = p.length();
+       vector <vector<int>> dp(n+1, vector<int>(m+1, -1)); 
+        return solve(s, p, s.length()-1,  p.length() -1, dp); 
+    }
+};
